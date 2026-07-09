6c481809f16281190cf2d14bb7597f8247b804d1
jimj90201-a11y <jimj90201@gmail.com>
2026-07-09 16:55:52 +0530

    Create PlayerRenderEvents.java

---
+++ src/main/java/com/gamerofpro/therage/event/PlayerRenderEvents.java
@@ -0,0 +1,33 @@
+package com.saksham.therage.event;
+
+import com.saksham.therage.manager.HeadlessManager;
+import net.minecraft.client.model.PlayerModel;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderPlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(value = Dist.CLIENT)
+public class PlayerRenderEvents {
+
+    @SubscribeEvent
+    public static void onRenderPlayerPre(RenderPlayerEvent.Pre event) {
+        Player player = event.getEntity();
+
+        if (HeadlessManager.isHeadless(player)) {
+            PlayerModel<?> model = event.getRenderer().getModel();
+
+            model.head.visible = false;
+            model.hat.visible = false;
+        }
+    }
+
+    @SubscribeEvent
+    public static void onRenderPlayerPost(RenderPlayerEvent.Post event) {
+        PlayerModel<?> model = event.getRenderer().getModel();
+
+        model.head.visible = true;
+        model.hat.visible = true;
+    }
+}
