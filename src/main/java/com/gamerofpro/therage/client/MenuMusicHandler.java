94024d2b3a47e6b20b8fac70439e3a966d4b66fe
jimj90201-a11y <jimj90201@gmail.com>
2026-07-11 17:40:38 +0530

    Fix sound instance location for MenuMusicHandler

--- src/main/java/com/gamerofpro/therage/client/MenuMusicHandler.java
+++ src/main/java/com/gamerofpro/therage/client/MenuMusicHandler.java
@@ -25,7 +25,7 @@ public class MenuMusicHandler {
 
                 mc.getSoundManager().play(
                         new SimpleSoundInstance(
-                                ModSounds.TH.get(),
+                                ModSounds.TH.get().getLocation(),
                                 SoundSource.MUSIC,
                                 1.0F,
                                 1.0F,
